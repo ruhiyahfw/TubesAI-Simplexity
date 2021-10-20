eb8d40fad2de4d3d76593e753b05aa2e5bcd409c
nabilahann <13519097@std.stei.itb.ac.id>
2021-10-20 12:39:44 +0700

    ubah run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python main.py --row 6 --col 7 --type bvb --player_choice 0 --thinking_time 2 --bot1 Hmm_minimax.pkl --bot2 Hmm_local_search.pkl
+python main.py --row 6 --col 7 --type bvb --player_choice 0 --thinking_time 3 --bot1 Hmm_minimax.pkl --bot2 Hmm_local_search.pkl
